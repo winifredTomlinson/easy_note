bdca077bc0b76532a9343b300fcdcdc14a621e7d
Jay.M.Hu <hm910705@163.com>
2016-12-15 10:29:18 +0800

    Remove layer

--- src/newkit/nk-core/services/negAlert.ts
+++ src/newkit/nk-core/services/negAlert.ts
@@ -3,41 +3,6 @@ import { NegUtil } from './negUtil';
 
 let noop = function () { };
 
-export interface LayerOptions {
-  type?: number; //【0】类型
-  title?: string | boolean; //【'信息'】标题，false不显示标题
-  content?: string | HTMLElement | Array<string>; // 【''】内容
-  skin?: string; //【''】主题
-  area?: string | Array<string>; //【'auto'】大小
-  offset?: string | Array<string>; //【'auto'】位置，特殊位置: offset: 'rb'
-  icon?: string; //【-1】图标（注意：插件本身是数字）
-  btn?: string | Array<string>; //【'确认'】按钮
-  closeBtn?: string | boolean; //【1】关闭按钮风格，设置false则不显示
-  shade?: string | Array<any> | boolean; //【0.3】遮罩颜色
-  shadeClose?: boolean; //【false】是否点击遮罩关闭
-  time?: number; //【0】单位s（注意，插件本身是毫秒）
-  id?: string; // 【''】设置之后，同一ID只能弹出一个
-  shift?: number; //【0】 弹出动画
-  maxmin?: boolean; //【false】是否允许最大最小化
-  fix?: boolean; //【true】 固定位置（不跟随滚动）
-  scrollbar?: boolean; //【true】是否允许浏览器滚动
-  maxWidth?: number; //【360】最大宽度，当aera:'auto'时才有效
-  zIndex?: number; //【19891014】z-index层次
-  move?: string | HTMLElement | boolean; //【'.layui-layer-title'】触发拖拽的元素
-  moveType?: number; //【0】拖拽风格
-  moveOut?: number; //【false】是否允许拖动到窗口外
-  moveEnd?: Function; //【null】 拖动完毕之后的回调方法
-  success?: Function; // 层弹出完毕
-  yes?: Function; //点击了确定按钮回调
-  cancel?: Function; //点击了取消按钮回调
-  end?: Function; //层销毁
-  btn1?: Function;
-  btn2?: Function;
-  btn3?: Function;
-  btn4?: Function;
-  btn5?: Function;
-}
-
 const defaults = {
   type: 'info',
   theme: 'flat',
@@ -46,16 +11,7 @@ const defaults = {
   escapeText: true
 };
 
-let getOptions = (options: LayerOptions, defaults?: any) => {
-  let opt = _.extend({}, defaults || {}, options);
-  if (opt.icon !== undefined) {
-    
-  }
-  if (opt.time !== undefined) {
-    opt.time *= 1000;
-  }
-  return opt;
-};
+declare let Messenger: any;
 
 @Injectable()
 export class NegAlert {
@@ -84,13 +40,45 @@ export class NegAlert {
     return this._post(opt);
   }
 
+  alert(message: string, callback?: Function, userOpt = {}) {
+    let opt = Object.assign({}, userOpt, {
+      message,
+      type: 'error',
+      showCloseButton: false,
+      seconds: 60 * 60 * 24,
+      actions: {
+        OK: callback || noop
+      }
+    });
+    return this._post(opt);
+  }
+
+  confirm(message: string, okCallback?: Function, cancelCallback?: Function, userOpt = {}) {
+    let opt = Object.assign({}, userOpt, {
+      message,
+      type: 'info',
+      showCloseButton: false,
+      seconds: 60 * 60 * 24,
+      actions: {
+        OK: okCallback || noop,
+        CANCEL: cancelCallback || noop
+      }
+    });
+    return this._post(opt);
+  }
+
+  show(message: string, userOpt = {}) {
+    let opt = Object.assign({ type: 'info' }, userOpt, { message });
+    return this._post(opt);
+  }
+
   _post(options: any) {
-    let opt = Object.assign({}, defaults);
-    ['theme', 'closeButtonText', 'onClickClose', 'message'].forEach(p => {
+    let opt: any = Object.assign({}, defaults);
+    ['type', 'theme', 'closeButtonText', 'onClickClose', 'message'].forEach(p => {
       opt[p] = options[p];
     });
     if (options.showCloseButton === false) {
-      options.showCloseButton = false;
+      opt.showCloseButton = false;
     }
     if (options.allowHtml) {
       opt.escapeText = false;
@@ -98,7 +86,22 @@ export class NegAlert {
     if (options.seconds) {
       opt.hideAfter = options.seconds;
     }
-    console.log(opt);
+    if (options.actions) {
+      opt.actions = {};
+      Object.keys(options.actions).forEach((label, idx) => {
+        opt.actions[`btn${idx + 1}`] = {
+          label,
+          action(evt, msg) {
+            Promise.resolve(options.actions[label](msg))
+              .then(notCancel => {
+                if (notCancel !== false) {
+                  msg.hide();
+                }
+              });
+          }
+        }
+      });
+    }
     let ins = this.messageBox.post(opt);
     if (typeof options.callback === 'function') {
       this.negUtil.addWatcher(ins, 'shown', newVal => {
@@ -106,39 +109,6 @@ export class NegAlert {
       }, ins.shown);
     }
     return ins;
-  }
-
-  alert(content, options?: LayerOptions, okCallback?: Function) {
-    return layer.alert(content, getOptions(options, { title: 'Message' }), okCallback);
-  }
-
-  confirm(content, options?: LayerOptions, okCallback?: Function, cancelCallback?: Function) {
-    return layer.confirm(content, getOptions(options, {
-      title: 'Confirm',
-      icon: 'question'
-    }), okCallback || noop, cancelCallback || noop)
-  }
-
-  notice(content, options?: any, callback?: Function) {
-    return this.open(getOptions(options, {
-      type: 1,
-      title: false,
-      closeBtn: false, //不显示关闭按钮
-      shade: [0],
-      area: options.area || ['340px', '215px'],
-      offset: 'rb', //右下角弹出
-      time: options.time || 3, //2秒后自动关闭
-      shift: 2,
-      content: content, //iframe的url，no代表不显示滚动条
-      end: () => {
-        (callback || noop)();
-      }
-    }), true);
-  }
-
-  open(options: LayerOptions, notProcessOptions?: boolean) {
-    let opt = notProcessOptions ? options : getOptions(options)
-    return layer.open(opt);
   }
 
   close(ins) {
