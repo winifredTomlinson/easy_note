584e5d2a0c85e161d14fa6085d4a7d36e5e62916
Jay.M.Hu <hm910705@163.com>
2016-12-28 15:54:03 +0800

    Menu setting page

--- src/newkit/nk-shell/app/menu-setting/menu-setting.component.ts
+++ src/newkit/nk-shell/app/menu-setting/menu-setting.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { NegAjax, NegAuth } from 'newkit/core';
+import { NegAjax, NegAuth, NegAlert } from 'newkit/core';
 
 @Component({
-  templateUrl: './menu-setting.html'
+  templateUrl: './menu-setting.html',
+  styleUrls: ['./menu-setting.css']
 })
 export class MenuSettingComponent implements OnInit {
 
   private menuData: Array<any> = [];
   private ajaxOpt: {};
+  private isModalShown: boolean = false;
+
   constructor(
     private negAjax: NegAjax,
-    private negAuth: NegAuth
+    private negAuth: NegAuth,
+    private negAlert: NegAlert
   ) { }
 
   ngOnInit() {
@@ -18,37 +22,61 @@ export class MenuSettingComponent implements OnInit {
     this._loadMenu();
   }
 
+  doMenuOperate(evt, opType, menu) {
+    evt.stopPropagation();
+    switch (opType) {
+      case 'edit':
+        this.doMenuEdit(menu);
+        break;
+      case 'addChild':
+        this.doMenuAdd(menu);
+        break;
+      case 'delete':
+        this.doMenuDelete(menu);
+        break;
+    }
+  }
+
+  doMenuAdd(menu) {
+    this.isModalShown = true;
+  }
+
+  doMenuEdit(menu) {
+
+  }
+
+  saveMenu(): Promise<any> {
+    alert('add');
+    return Promise.resolve(false);
+  }
+
+  doMenuDelete(menu) {
+    this.negAlert.confirm('Sure to delete the menu?', () => {
+      this.negAjax.delete(`${NewkitConf.NewkitAPI}/menu/${menu.id}`, this.ajaxOpt)
+        .then(data => {
+          this._loadMenu();
+          this.negAlert.info('Delete menu successfully.');
+        });
+    }, null);
+  }
+
   _loadMenu() {
     this.negAjax.get(`${NewkitConf.NewkitAPI}/menu`, this.ajaxOpt)
       .then(res => {
         let menus = res.json().data.rows;
-        console.log(menus);
-        this.menuData = menus;
-        // let menuData = res.json().menuData;
-        // menuData.forEach((item) => item.$$root = true);
-        // this.menuData = this._convertMenuDataToTreeData(menuData);
-        // console.log(this.menuData);
+        let result = [];
+        this.processMenuData(menus, result, '');
+        this.menuData = result;
+        console.log(result);
       })
   }
 
-  _convertMenuDataToTreeData(menuData): Array<any> {
-    let treeData = [];
-    if (menuData && menuData.length > 0) {
-      menuData.forEach(item => {
-        let menu: any = {
-          data: {}
-        };
-        Object.keys(item).forEach(key => {
-          if (key !== 'SubMenus') {
-            menu.data[key] = item[key];
-          }
-        });
-        if (item.SubMenus) {
-          menu.children = this._convertMenuDataToTreeData(item.SubMenus);
-        }
-        treeData.push(menu);
-      });
-    }
-    return treeData;
+  processMenuData(source, target, parentId, level = 0): void {
+    let arr = source.filter(x => x.parentId === parentId);
+    arr.forEach(m => {
+      m.level = level;
+      target.push(m);
+      this.processMenuData(source, target, m.id, level + 1);
+    });
   }
 }
